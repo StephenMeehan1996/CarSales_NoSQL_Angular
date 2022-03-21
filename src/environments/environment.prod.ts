4f246969e597291bd12ee3436ae74225235a0af3
StephenMeehan1996 <78025435+StephenMeehan1996@users.noreply.github.com>
2022-03-21 12:10:49 +0000

    Added FireBase, Added Service, Added Interface

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+   firebase: {
+    apiKey: "AIzaSyDZdX-SFnowVj0DD47LGAHfiHPbBJgtowo",
+    authDomain: "carsales-26eaa.firebaseapp.com",
+    projectId: "carsales-26eaa",
+    storageBucket: "carsales-26eaa.appspot.com",
+    messagingSenderId: "1008452442331",
+    appId: "1:1008452442331:web:0c27740e3fa3e46bb1d4fe",
+    measurementId: "G-QKZHEWDN7T"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,21 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDZdX-SFnowVj0DD47LGAHfiHPbBJgtowo",
+    authDomain: "carsales-26eaa.firebaseapp.com",
+    projectId: "carsales-26eaa",
+    storageBucket: "carsales-26eaa.appspot.com",
+    messagingSenderId: "1008452442331",
+    appId: "1:1008452442331:web:0c27740e3fa3e46bb1d4fe",
+    measurementId: "G-QKZHEWDN7T"
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
